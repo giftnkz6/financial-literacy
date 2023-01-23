278bed2a7da79ab96c1b0fd90eaf87beb9620561
giftnkz6 <giftnkz6@gmail.com>
2023-01-23 16:25:28 +0200

    Added a function

--- js/index.js
+++ js/index.js
@@ -2,5 +2,10 @@ let kids = document.querySelector('.kids');
 
 kids.addEventListener('click', (e) => {
     alert("Cartoons")
+    kidsSite()
 }
 )
+
+function kidsSite(){
+    document.write('Welcome Kiddo')
+}
